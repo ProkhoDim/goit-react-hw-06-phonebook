067aab11b53480001a3d6f72b4924115764632c4
Dmitrij Prokhorenko <dmitrij.prokhorenko@gmail.com>
2020-08-07 22:11:27 +0300

    Updates

---
+++ precache-manifest.fa04625ec4f081473b5194bbfe9b848c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63233a306eb8b6071d66d79d292f3069",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "978d0836fcab6419d97d",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/static/css/main.5a84bc43.chunk.css"
+  },
+  {
+    "revision": "fb7b6fe7712949e6f512",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/static/js/2.91ea9c7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/static/js/2.91ea9c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "978d0836fcab6419d97d",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/static/js/main.73321166.chunk.js"
+  },
+  {
+    "revision": "15e4488b2966e97c1f62",
+    "url": "/ProkhoDim/goit-react-hw-06-phonebook/static/js/runtime-main.6ea6f191.js"
+  }
+]);
